ecc30c54e762b3f9d2fc02d50b9cf4a83322a50e
MatheusBorgez <matheusab594@gmail.com>
2024-07-10 02:10:30 -0300

    altera porta

--- src/index.js
+++ src/index.js
@@ -8,6 +8,6 @@ app.use(express.json())
 
 app.use(routes)
 
-app.listen(3000, () => {
-  console.log(`Example app listening on port ${3000}`)
+app.listen(3001, () => {
+  console.log(`Example app listening on port ${3001}`)
 })
